febcfde310a329b89b843b64ae19408c6864bda0
Shawn <shawn@fidesinnova.io>
2024-11-20 14:06:36 +0330

    Update index.jsx

--- frontend/src/components/ui/Button/index.jsx
+++ frontend/src/components/ui/Button/index.jsx
@@ -1,15 +1,31 @@
-import React from "react";
-import "./style.scss";
+import React from 'react';
+import './style.scss';
 
-export default function Button({ className, children, loading, ...props }) {
+export default function Button({
+	className,
+	children,
+	loading,
+	onClick,
+	...props
+}) {
+	const handleClick = (e) => {
+		if (loading == true) {
+			e.preventDefault(); // Prevent the click if loading
+			return false;
+		}
+		if (onClick) {
+			onClick(e); // Call the onClick handler if not loading
+		}
+	};
+
 	return (
 		<div
 			{...props}
-			className={`${className} custom-button ${loading ? "loading" : ""}`}
-			disabled={loading}
+			className={`${className} custom-button ${loading ? 'loading' : ''}`}
+			onClick={handleClick}
 		>
 			{loading && <div className="loader"></div>}
-			<p className={loading ? "text-loading" : ""}>{children}</p>
+			<p className={loading ? 'text-loading' : ''}>{children}</p>
 		</div>
 	);
 }
